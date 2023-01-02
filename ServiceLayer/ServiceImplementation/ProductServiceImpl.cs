26a8e456c90769738e80a3b52dfbf5c8db25678a
Arhip Florin <65599211+Florin9925@users.noreply.github.com>
2023-01-02 15:35:28 +0200

    Added product and category services

---
+++ ServiceLayer/ServiceImplementation/ProductServiceImpl.cs
@@ -0,0 +1,37 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceImplementation
+{
+    public class ProductServiceImpl : IProductService
+    {
+        void ICRUDService<Product>.Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        IList<Product> ICRUDService<Product>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICRUDService<Product>.GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICRUDService<Product>.Insert(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICRUDService<Product>.Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
