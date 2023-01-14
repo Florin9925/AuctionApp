cf997f9a1c4da02114883ea5499979b9fe33a002
Arhip Florin <65599211+Florin9925@users.noreply.github.com>
2023-01-14 16:52:13 +0200

    Added StringDistanceTest

---
+++ TestServiceLayer/StringDistanceTest.cs
@@ -0,0 +1,27 @@
+﻿using ServiceLayer.Utils;
+
+namespace TestServiceLayer;
+
+[TestClass]
+public class StringDistanceTest
+{
+    private string description = "This is a test of the string distance algorithm";
+
+    private string descriptionSimilar = "This is a test of the string distance algorithm 2";
+
+    private string descriptionDifferent = "This is't a description of a sequence of characters";
+    
+    [TestMethod]
+    public void TestStringDistanceSimilar()
+    {
+        var distance = StringDistance.LevenshteinDistance(description, descriptionSimilar);
+        Assert.IsTrue(distance < 10);
+    }
+    
+    [TestMethod]
+    public void TestStringDistanceDifferent()
+    {
+        var distance = StringDistance.LevenshteinDistance(description, descriptionDifferent);
+        Assert.IsTrue(distance > 10);
+    }
+}
